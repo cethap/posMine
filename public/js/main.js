2e13a4ac6ba2bc63756cb9788fa93ca101a5e9e1
Cesar Tapasco <cethapgames@gmail.com>
2017-08-29 22:10:46 -0500

    Nuevos cambios y arreglos

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,7 @@ var app = new Vue({
     },
     mounted: function() {
         this.getProducts();
+        $("#barcode").focus();
         if (!localStorage.getItem("productsInv")) {
             localStorage.setItem("productsInv", '[]');
         }
@@ -30,11 +31,17 @@ var app = new Vue({
                     var e = self.products[i];
                     var search = true;
                     for (var y = 0; search && y < pro.length; y++) {
-                        if (pro[y].id == e.id) {
-                            e.qty = pro[y].qty;
-                            e.list_price = pro[y].list_price;
+                        if(!pro[y].id){
+                            self.products.push(pro[y]);
                             pro.splice(y, 1);
-                            search = false;
+                        }
+                        if(pro.length-1 > i){
+                            if (pro[y].id == e.id) {
+                                e.qty = pro[y].qty;
+                                e.list_price = pro[y].list_price;
+                                pro.splice(y, 1);
+                                search = false;
+                            }
                         }
                     }
                 }
@@ -42,20 +49,50 @@ var app = new Vue({
                 pro = null;
             });
         },
-        addproduct: function() {
-            this.edit = true;
+        removeProduct: function(indx,ev){
+            ev.preventDefault();
+            ev.stopImmediatePropagation();
+            var el = this.products.splice(indx,1);
             var pro = JSON.parse(localStorage.getItem("productsInv"));
-            pro.push(this.Nproduct);
+
+            function findProduct(product, index) { 
+                if(product){
+                    if(product.product === el[0].product){
+                        pro.splice(index,1);
+                    }
+                }
+            }
+            
+            pro.find(findProduct)
             localStorage.setItem("productsInv", JSON.stringify(pro));
-            pro = null;
-            this.products.push(JSON.parse(JSON.stringify(this.Nproduct)));
-            this.Nproduct = {};
+        },
+        setNewMode: function(){
+            this.edit = false;
+            this.Nproduct.qty = '0';
+            this.Nproduct.list_price = '0';
+            Vue.nextTick(function() {
+                $("#barcodeN").focus();
+            });            
+        },
+        addproduct: function() {
+            if(this.Nproduct.product.length > 0 && (this.Nproduct.qty+'').length > 0 && (this.Nproduct.list_price+'').length > 0){
+                this.edit = true;
+                var pro = JSON.parse(localStorage.getItem("productsInv"));
+                pro.push(JSON.parse(JSON.stringify(this.Nproduct)));
+                localStorage.setItem("productsInv", JSON.stringify(pro));
+                pro = null;
+                this.products.push(JSON.parse(JSON.stringify(this.Nproduct)));
+                this.Nproduct = {};
+            }else{
+                alert("Debe llenar almenos nombre, cantidad y precio")
+            }
         },
         serQty: function() {
-            if (this.productSelected && this.qtyProduct.length > 0) {
+            if (this.productSelected && (this.qtyProduct+'').length > 0) {
 
                 this.currentProduct.qty = parseInt(this.qtyProduct, 10);
-
+                this.currentProduct.list_price = this.PriceProduct;
+                
                 var pro = JSON.parse(localStorage.getItem("productsInv"));
                 var found = false;
 
@@ -63,7 +100,7 @@ var app = new Vue({
                     var element = pro[index];
                     if (element.id == this.currentProduct.id) {
                         element.qty = parseInt(this.qtyProduct, 10);
-                        //element.list_price = this.PriceProduct;
+                        element.list_price = this.PriceProduct;
                         found = true;
                     }
                 }
@@ -78,7 +115,7 @@ var app = new Vue({
                 this.barcode = '';
                 this.nameProduct = '';
                 this.qtyProduct = '';
-                //this.PriceProduct = '';
+                this.PriceProduct = '';
                 this.enabledQty = false;
                 this.editQty = false;
 
@@ -92,10 +129,13 @@ var app = new Vue({
             }
         },
         exitQty: function() {
+            this.edit = true;
+            this.Nproduct = {};
+
             this.barcode = '';
             this.nameProduct = '';
             this.qtyProduct = '';
-            //this.PriceProduct = '';
+            this.PriceProduct = '';
             this.currentProduct = null;
             this.enabledQty = false;
             this.editQty = false;
@@ -107,15 +147,28 @@ var app = new Vue({
         },
         updateQty: function() {
             if (this.enabledQty) {
+                $("#barcode").focus();
+                $("#name").focus();
+                $("#qty").focus();
+                $("#price").focus();
                 this.editQty = true;
             }
         },
         setProduct: function(data) {
             this.barcode = data.barcode;
             this.nameProduct = data.product;
-            $("#barcode").focus();
-            $("#name").focus();
-        }
+
+            Vue.nextTick(function() {
+                $("#barcode").focus();
+                $("#name").focus();
+            });
+        },
+        newInventory: function() {
+            var self = this;
+            $.post("/new_inventary", { products: localStorage.getItem("productsInv") }, function(data) {
+
+            });
+        }        
     },
     computed: {
         productFilter: function() {
@@ -149,7 +202,7 @@ var app = new Vue({
                 self.barcode = products[0].barcode;
                 self.nameProduct = products[0].product;
                 self.qtyProduct = products[0].qty || '';
-                //self.PriceProduct = products[0].list_price || '';
+                self.PriceProduct = products[0].list_price || '';
 
             } else {
                 self.productSelected = false;
